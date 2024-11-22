3c22a7b8a885cbc5e70e6628b5702d30c20b1bf6
mati-gif <matiassoria7700@gmail.com>
2024-11-21 23:15:23 -0300

    añadiendo estilos

--- src/pages/CienciasSociales.jsx
+++ src/pages/CienciasSociales.jsx
@@ -6,11 +6,33 @@ import { Image } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 function CienciasSociales() {
-  return (
-    <>
-    <div class='cienciasSociales'>CienciasSociales</div>
-    </>
-  )
+    return (
+        <>
+            <div class='cienciasSociales'>
+                <section
+                    class="banner-cienciasSociales "
+                >
+                    <div
+                        class="simple-div__cienciasSociales "
+                    ></div>
+
+                    <div
+                        class="simple-div2--cienciasSociales "
+                    >
+                        <div class="divDentroBanner-cienciasSociales ">
+                            <h1 class="h1DentroBanner__cienciasSociales ">
+
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+                <Breadcrumb style={{ fontSize: "20px", marginLeft: "140px", marginTop: "20px" }}>
+                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                    <Breadcrumb.Item active>Engineering</Breadcrumb.Item>
+                </Breadcrumb>
+            </div>
+        </>
+    )
 }
 
 export default CienciasSociales
